a9d4c6532fa19f63ad582c28409fb5b2c55a34a5
CantTypeChat <shakay7777@gmail.com>
2025-06-09 22:09:14 +0900

    add warn test

---
+++ src/sem_warn_ass.c
@@ -0,0 +1,11 @@
+int main() {
+    int i;
+    float f;
+    i = 1;
+    i = 1.0;
+    f = 1;
+    f = 1.0;
+    return 0;
+}
+
+
---
+++ src/sem_warn_bin.c
@@ -0,0 +1,4 @@
+int main() {
+    printf("%f\n", 1 + 1.0);
+    return 0;
+}
